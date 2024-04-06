154382b3e3b8706c6df04489a17035feee3f2286
210 student <pythontest3@mail.ru>
2024-04-06 14:44:46 +0200

    Test

---
+++ keyboards/common_keyboards.py
@@ -0,0 +1,9 @@
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+
+def main_keyboard() -> ReplyKeyboardMarkup:
+    kb = ReplyKeyboardBuilder()
+    kb.button(text="Погода")
+    kb.button(text="Население стран")
+    kb.button(text="Население городов")
+    return kb.as_markup(resize_keyboard=True)
